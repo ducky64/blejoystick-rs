18459c4d7296111037bec878d5827ec3af48e91b
Richard Lin <elpato25@gmail.com>
2025-12-14 14:45:07 -0800

    Migrate code to latest TrouBLE example

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,17 +6,18 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use bt_hci::controller::ExternalController;
+// TrouBLE example imports
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
-use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
-use esp_hal::timer::systimer::SystemTimer;
 use esp_hal::timer::timg::TimerGroup;
-use esp_wifi::ble::controller::BleConnector;
+use esp_radio::ble::controller::BleConnector;
+use trouble_host::prelude::ExternalController;
+use esp_backtrace as _;
 
+// App-specific imports
+use embedded_hal_nb::nb;
+use embassy_time::{Duration, Timer};
 use esp_hal::{
-    delay::Delay,
     gpio::{Input, InputConfig, Pull, Level, Output, OutputConfig},
     analog::adc::{AdcConfig, Adc, Attenuation},
 };
@@ -28,36 +29,32 @@ extern crate alloc;
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
-#[esp_hal_embassy::main]
+#[esp_rtos::main]
 async fn main(spawner: Spawner) {
-    // generator version: 0.5.0
+    // based on examples from https://github.com/embassy-rs/trouble/tree/main/examples/esp32
+    // in particular ble_bas_peripheral.rs
+    esp_println::logger::init_logger_from_env();
+    let peripherals = esp_hal::init(esp_hal::Config::default().with_cpu_clock(CpuClock::max()));
+    esp_alloc::heap_allocator!(size: 72 * 1024);
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    #[cfg(target_arch = "riscv32")]
+    let software_interrupt = esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
 
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let peripherals = esp_hal::init(config);
+    esp_rtos::start(
+        timg0.timer0,
+        #[cfg(target_arch = "riscv32")]
+        software_interrupt.software_interrupt0,
+    );
 
-    esp_alloc::heap_allocator!(size: 64 * 1024);
-    // COEX needs more RAM - so we've added some more
-    esp_alloc::heap_allocator!(#[unsafe(link_section = ".dram2_uninit")] size: 64 * 1024);
+    let radio = esp_radio::init().unwrap();
+    let bluetooth = peripherals.BT;
+    let connector = BleConnector::new(&radio, bluetooth, Default::default()).unwrap();
+    let controller: ExternalController<_, 20> = ExternalController::new(connector);
 
-    let timer0 = SystemTimer::new(peripherals.SYSTIMER);
-    esp_hal_embassy::init(timer0.alarm0);
-
-    let rng = esp_hal::rng::Rng::new(peripherals.RNG);
-    let timer1 = TimerGroup::new(peripherals.TIMG0);
-    let wifi_init =
-        esp_wifi::init(timer1.timer0, rng).expect("Failed to initialize WIFI/BLE controller");
-    let (mut _wifi_controller, _interfaces) = esp_wifi::wifi::new(&wifi_init, peripherals.WIFI)
-        .expect("Failed to initialize WIFI controller");
-    // find more examples https://github.com/embassy-rs/trouble/tree/main/examples/esp32
-    let transport = BleConnector::new(&wifi_init, peripherals.BT);
-    let _ble_controller = ExternalController::<_, 20>::new(transport);
-
-    // TODO: Spawn some tasks
-    let _ = spawner;
-
+    // App-specific code starts here
     println!("Quack quack quack world");
 
-    let mut led = Output::new(peripherals.GPIO9, Level::Low, OutputConfig::default());
+    let led = Output::new(peripherals.GPIO9, Level::Low, OutputConfig::default());
     let button = Input::new(peripherals.GPIO6, InputConfig::default().with_pull(Pull::Up));
 
     let mut adc_config = esp_hal::analog::adc::AdcConfig::new();
@@ -75,7 +72,7 @@ async fn main(spawner: Spawner) {
         let joystick_y_value = nb::block!(adc.read_oneshot(&mut joystick_y_pin)).unwrap();
         let trig_value = nb::block!(adc.read_oneshot(&mut trig_pin)).unwrap();
         let vbat_value = nb::block!(adc.read_oneshot(&mut vbat_pin)).unwrap();
-        println!("JX {joystick_x_value}    JY {joystick_y_value}    T {trig_value}    VB {vbat_value}");
+        println!("JX {joystick_x_value}    JY {joystick_y_value}    Tr {trig_value}    VB {vbat_value}");
         Timer::after(Duration::from_millis(100)).await;
     }
 }
