a5a2ff5ed5d4ddf83da3d84ed9b64e6895f2abca
Richard Lin <elpato25@gmail.com>
2026-08-05 01:47:32 -0700

    moving

--- src/battery.rs
+++ src/battery.rs
@@ -8,6 +8,7 @@ type I2cBus = Mutex<CriticalSectionRawMutex, Twim<'static>>;
 
 use crate::bus::GlobalBus;
 use crate::prelude::*;
+use crate::util::interpolate_1d;
 use ina219::address::Address;
 use ina219::calibration::{IntCalibration, MicroAmpere};
 use ina219::configuration::{Configuration, MeasuredSignals, OperatingMode};
@@ -23,31 +24,6 @@ const LIPO_V_SOC: [(u16, u16); 7] = [
     (4000, 84),
     (4200, 100),
 ];
-
-// it turns out a bunch of interp crates are not no_std
-use core::ops::{Add, Div, Mul, Sub};
-fn interpolate_1d<X, Y>(breakpoints: &[(X, Y)], x: X) -> Y
-where
-    X: PartialOrd + Copy + Sub<Output = X>,
-    Y: Copy + Add<Output = Y> + Sub<Output = Y> + Mul<X, Output = Y> + Div<X, Output = Y>,
-{
-    assert!(!breakpoints.is_empty(), "breakpoints slice cannot be empty");
-
-    for window in breakpoints.windows(2) {
-        let (x0, y0) = window[0];
-        let (x1, y1) = window[1];
-
-        if x < x0 {
-            return y0; // clamp to lowest
-        } else if x >= x0 && x <= x1 {
-            let dx = x1 - x0;
-            let dy = y1 - y0;
-            let offset = x - x0;
-            return y0 + (dy * offset) / dx;
-        }
-    }
-    breakpoints.last().unwrap().1 // clamp to highest
-}
 
 #[embassy_executor::task]
 pub(crate) async fn battery_task(
--- src/util.rs
+++ src/util.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 
 // LinearMap does not have a defmt Format impl, this wrapper provides defmt support
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct FormatLinearMap<K: core::cmp::Eq, V, const N: usize>(pub LinearMap<K, V, N>);
+pub(crate) struct FormatLinearMap<K: core::cmp::Eq, V, const N: usize>(pub LinearMap<K, V, N>);
 impl<'a, K: Format + core::cmp::Eq, V: Format, const N: usize> Format for FormatLinearMap<K, V, N> {
     fn format(&self, fmt: Formatter) {
         defmt::write!(fmt, "{{");
@@ -17,4 +17,29 @@ impl<'a, K: Format + core::cmp::Eq, V: Format, const N: usize> Format for Format
         }
         defmt::write!(fmt, "}}");
     }
+}
+
+// it turns out a bunch of interp crates are not no_std
+use core::ops::{Add, Div, Mul, Sub};
+pub(crate) fn interpolate_1d<X, Y>(breakpoints: &[(X, Y)], x: X) -> Y
+where
+    X: PartialOrd + Copy + Sub<Output = X>,
+    Y: Copy + Add<Output = Y> + Sub<Output = Y> + Mul<X, Output = Y> + Div<X, Output = Y>,
+{
+    assert!(!breakpoints.is_empty(), "breakpoints slice cannot be empty");
+
+    for window in breakpoints.windows(2) {
+        let (x0, y0) = window[0];
+        let (x1, y1) = window[1];
+
+        if x < x0 {
+            return y0; // clamp to lowest
+        } else if x >= x0 && x <= x1 {
+            let dx = x1 - x0;
+            let dy = y1 - y0;
+            let offset = x - x0;
+            return y0 + (dy * offset) / dx;
+        }
+    }
+    breakpoints.last().unwrap().1 // clamp to highest
 }
