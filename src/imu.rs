7bff13fc7cbe1026892fce80325b96c23602e3fa
Richard Lin <elpato25@gmail.com>
2026-08-08 00:27:56 -0700

    skeleton imu driver

--- src/imu.rs
+++ src/imu.rs
@@ -6,11 +6,29 @@ use embassy_time::Timer;
 type I2cBus = Mutex<CriticalSectionRawMutex, Twim<'static>>;
 
 use crate::bus::GlobalBus;
+use crate::lsm6ds3tr::Lsm6ds3tr;
 use crate::prelude::*;
 
 #[embassy_executor::task]
 pub(crate) async fn imu_task(bus: &'static GlobalBus, i2c_bus: &'static I2cBus) {
-    // let mut imu = Lsm6ds33::new(I2cDevice::new(&i2c_bus), 0x6A).await.unwrap();
+    let mut imu = match async {
+        let mut dev = Lsm6ds3tr::new(I2cDevice::new(&i2c_bus));
+        // let id = dev.read_whoami().await.map_err(|_| ())?;
+        // info!("IMU: got id {}", id);
+        if !dev.check().await.map_err(|_| ())? {
+            warn!("IMU check failed"); // TODO should be fatal
+        }
+        Ok::<_, ()>(dev)
+    }
+    .await
+    {
+        Ok(dev) => dev,
+        Err(_) => {
+            error!("IMU: failed to initialize");
+            return;
+        }
+    };
+
     info!("IMU: initialized");
     loop {
         Timer::after_millis(1000).await;
--- src/lsm6ds3tr.rs
+++ src/lsm6ds3tr.rs
@@ -1,49 +1,82 @@
 use bilge::prelude::*;
-use embedded_hal_async::i2c::{I2c, Operation};
+use embedded_hal_async::i2c::I2c;
 
 // TODO separate out into own repo
 
 /// Transport layer that supports both SPI and I2C
-trait Transport {
+pub trait Transport {
     type Error;
     async fn write(&mut self, addr: u8, data: &[u8]) -> Result<(), Self::Error>;
     async fn read(&mut self, addr: u8, buffer: &mut [u8]) -> Result<(), Self::Error>;
 }
 
-pub struct I2cTransport<I2CType> {
-    i2c: I2CType,
+pub struct I2cTransport<I2cType> {
+    i2c: I2cType,
     address: u8,
 }
 
-impl<I2CType> Transport for I2cTransport<I2CType>
+impl<I2cType> Transport for I2cTransport<I2cType>
 where
-    I2CType: I2c,
+    I2cType: I2c,
 {
-    type Error = I2CType::Error;
+    type Error = I2cType::Error;
 
-    async fn write(&mut self, addr: u8, data: &[u8]) -> Result<(), I2CType::Error> {
+    async fn write(&mut self, addr: u8, data: &[u8]) -> Result<(), I2cType::Error> {
         self.i2c.write(self.address, &[addr]).await?;
         self.i2c.write(self.address, data).await?;
         Ok(())
     }
 
-    async fn read(&mut self, addr: u8, buffer: &mut [u8]) -> Result<(), I2CType::Error> {
-        self.i2c.write(self.address, &[addr]).await?;
-        self.i2c.read(self.address, buffer).await?;
+    async fn read(&mut self, addr: u8, buffer: &mut [u8]) -> Result<(), I2cType::Error> {
+        self.i2c.write_read(self.address, &[addr], buffer).await?;
         Ok(())
     }
 }
 
-pub struct Lsm6ds3tr<TransportType> {
+pub struct Lsm6ds3tr<TransportType>
+where
+    TransportType: Transport,
+{
     transport: TransportType,
 }
 
-impl<I2CType> Lsm6ds3tr<I2cTransport<I2CType>> {
-    pub fn new(i2c: I2CType, address: u8) -> Self {
+impl<I2cType> Lsm6ds3tr<I2cTransport<I2cType>>
+where
+    I2cType: I2c,
+{
+    const LSM6DS3TR_ID: u8 = 0x6A; // SA0 = 0
+
+    /// Creates a device with address with SA0=0
+    pub fn new(i2c: I2cType) -> Self {
         Self {
-            transport: I2cTransport { i2c, address },
+            transport: I2cTransport {
+                i2c,
+                address: Self::LSM6DS3TR_ID,
+            },
         }
     }
 }
 
-impl<TransportType> Lsm6ds3tr<TransportType> {}
+#[repr(u8)]
+enum RegisterAddress {
+    WhoAmI = 0x0F,
+}
+
+impl<TransportType> Lsm6ds3tr<TransportType>
+where
+    TransportType: Transport,
+{
+    const WHO_AM_I_ID: u8 = 0b01101010;
+
+    pub async fn read_whoami(&mut self) -> Result<u8, TransportType::Error> {
+        let mut buffer = [0u8; 1];
+        self.transport
+            .read(RegisterAddress::WhoAmI as u8, &mut buffer)
+            .await?;
+        Ok(buffer[0])
+    }
+
+    pub async fn check(&mut self) -> Result<bool, TransportType::Error> {
+        Ok(self.read_whoami().await? == Self::WHO_AM_I_ID)
+    }
+}
